b15f23918ebf557bce684b55593e5e65986614cb
Finding Fortune <hulitaphy@gmail.com>
2026-03-28 17:39:22 -0400

    Fix formatting

--- src/GridDebug.cpp
+++ src/GridDebug.cpp
@@ -7,23 +7,23 @@
 
 void Grid::DrawGridDebug()
 {
-    for(int dx=-2;dx<=2;dx++)
-    for(int dy=-2;dy<=2;dy++)
+    for(int dx= -2; dx <= 2; dx++)
+    for(int dy= -2; dy <= 2; dy++)
     {
-        if(dx||dy)
+        if(dx || dy)
         {
-            DrawText(FMT("CA Iterations: {}. (-, =)", CAiterations),10+dx,35+dy,36,BLACK);
-            DrawText(FMT("Seed: {}. (o, p)", seed),10+dx,75+dy,36,BLACK);
-            DrawText(FMT("Grid Length: {}. (k, l)", gridLength),10+dx,105+dy,36,BLACK);
-            DrawText(FMT("Drawing {} caves", (renderCACaves == true ? "CA" : "FastNoise2")),10+dx,145+dy,36,BLACK);
-            DrawText(FMT("Generation Time: {}ms", STRP((generationTime * 1000.0), 2)),10+dx,185+dy,36,BLACK);
+            DrawText(FMT("CA Iterations: {}. (-, =)", CAiterations), 10  +dx, 35 + dy, 36, BLACK);
+            DrawText(FMT("Seed: {}. (o, p)", seed), 10 + dx, 75 + dy, 36, BLACK);
+            DrawText(FMT("Grid Length: {}. (k, l)", gridLength), 10 + dx, 105 + dy, 36, BLACK);
+            DrawText(FMT("Drawing {} caves", (renderCACaves == true ? "CA" : "FastNoise2")), 10 + dx, 145 + dy, 36, BLACK);
+            DrawText(FMT("Generation Time: {}ms", STRP((generationTime * 1000.0), 2)), 10 + dx, 185 + dy, 36, BLACK);
         }
 
-        DrawText(FMT("CA Iterations: {}. (-, =)", CAiterations),10,35,36,RAYWHITE);
-        DrawText(FMT("Seed: {}. (o, p)", seed),10,75,36,RAYWHITE);
-        DrawText(FMT("Grid Length: {}. (k, l)", gridLength),10,105,36,RAYWHITE);
-        DrawText(FMT("Drawing {} caves", (renderCACaves == true ? "CA" : "FastNoise2")),10,145,36,RAYWHITE);
-        DrawText(FMT("Generation Time: {}ms", STRP((generationTime * 1000.0), 2)),10,185,36,RAYWHITE);
+        DrawText(FMT("CA Iterations: {}. (-, =)", CAiterations), 10, 35, 36, RAYWHITE);
+        DrawText(FMT("Seed: {}. (o, p)", seed), 10, 75, 36, RAYWHITE);
+        DrawText(FMT("Grid Length: {}. (k, l)", gridLength), 10, 105, 36, RAYWHITE);
+        DrawText(FMT("Drawing {} caves", (renderCACaves == true ? "CA" : "FastNoise2")), 10, 145, 36, RAYWHITE);
+        DrawText(FMT("Generation Time: {}ms", STRP((generationTime * 1000.0), 2)), 10, 185, 36, RAYWHITE);
     }
 
     static Timer SettingsChangeTimer(0.25);
